8a3dd5cf0cbc7d479800e5220a7666aa500f22e2
B2B Stock App <dev@b2bstock.app>
2026-05-18 02:06:09 +0530

    fix: import go_router in analytics screen to resolve context.push compilation issue

--- mobile/lib/presentation/screens/analytics/analytics_screen.dart
+++ mobile/lib/presentation/screens/analytics/analytics_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:go_router/go_router.dart';
 import '../../../core/constants/app_constants.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../providers/products_provider.dart';
